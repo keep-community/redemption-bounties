5359180f2c42830952ea48f68f7dedefad5d4faf
corollari <github@albert.sh>
2020-12-29 06:30:56 +0100

    Add basic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './farmer.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Time to farm
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
