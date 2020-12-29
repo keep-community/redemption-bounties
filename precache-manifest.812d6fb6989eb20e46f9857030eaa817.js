b358263dd793acb047d222beeff71d1e47500879
corollari <corollari@users.noreply.github.com>
2020-12-29 08:50:37 +0000

    deploy: ba95b68ae6d3acee25623f63f429160050d4ed34

---
+++ precache-manifest.812d6fb6989eb20e46f9857030eaa817.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2bccfa69e382c7d0a6b1daab2e3a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a0303d9bc34582a2f0",
+    "url": "/static/css/main.4708bbbc.chunk.css"
+  },
+  {
+    "revision": "6396e8c127d4ea368921",
+    "url": "/static/js/2.befc69aa.chunk.js"
+  },
+  {
+    "revision": "a63ce6e57014fff1c27ab5dab5054867",
+    "url": "/static/js/2.befc69aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a0303d9bc34582a2f0",
+    "url": "/static/js/main.df28c4a5.chunk.js"
+  },
+  {
+    "revision": "ac36d267f572fb2fb627",
+    "url": "/static/js/runtime-main.f18cd915.js"
+  },
+  {
+    "revision": "332a7222462aa5f8cfbd073904dde6f1",
+    "url": "/static/media/apercu-bold-pro.332a7222.woff"
+  },
+  {
+    "revision": "60231e471dcef4f404c9491ffc4f3003",
+    "url": "/static/media/apercu-bold-pro.60231e47.eot"
+  },
+  {
+    "revision": "85484227ca3ec4355e25ec8fc0c84794",
+    "url": "/static/media/apercu-bold-pro.85484227.woff2"
+  },
+  {
+    "revision": "b3e904d47dcf5fe7fb3b43a1e931a0c5",
+    "url": "/static/media/apercu-bold-pro.b3e904d4.ttf"
+  },
+  {
+    "revision": "0f1a55d57bcb8f4fe1f626583a6a3300",
+    "url": "/static/media/apercu-mono-regular-pro.0f1a55d5.woff2"
+  },
+  {
+    "revision": "20fd6914cbf71b3b8db9c0412a0443b5",
+    "url": "/static/media/apercu-mono-regular-pro.20fd6914.eot"
+  },
+  {
+    "revision": "506052352cf18259139e5c3e50fc05a2",
+    "url": "/static/media/apercu-mono-regular-pro.50605235.ttf"
+  },
+  {
+    "revision": "a1c2d96578d2fc402dd7fe4a518b8d20",
+    "url": "/static/media/apercu-mono-regular-pro.a1c2d965.woff"
+  },
+  {
+    "revision": "16c89ca508b3d581fdc1c1876d0746bf",
+    "url": "/static/media/apercu-regular-pro.16c89ca5.ttf"
+  },
+  {
+    "revision": "6f3412ae4a2eb618d9dc486fc86d4a13",
+    "url": "/static/media/apercu-regular-pro.6f3412ae.woff"
+  },
+  {
+    "revision": "ad897f51f26a0018f7419a4a3ace42d7",
+    "url": "/static/media/apercu-regular-pro.ad897f51.eot"
+  },
+  {
+    "revision": "c015297063ed0e4129ca1226d9a357d5",
+    "url": "/static/media/apercu-regular-pro.c0152970.woff2"
+  },
+  {
+    "revision": "ed8b436ec2b9564bb6b403e310103f11",
+    "url": "/static/media/check.ed8b436e.svg"
+  },
+  {
+    "revision": "90ddd75d66d9b0cf359b4efbc26d32ea",
+    "url": "/static/media/warning.90ddd75d.svg"
+  }
+]);
